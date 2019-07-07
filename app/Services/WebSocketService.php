e7bdcb899c0bc271d2c2b765a0d976064fb6e6e6
xthon <1873300303@qq.com>
2019-07-07 16:41:10 +0800

    测试ws

--- app/Services/WebSocketService.php
+++ app/Services/WebSocketService.php
@@ -11,22 +11,29 @@ class WebSocketService implements WebSocketHandlerInterface
     // 声明没有参数的构造函数
     public function __construct()
     {
+
     }
+
+    //建立时触发
     public function onOpen(Server $server, Request $request)
     {
         // 在触发onOpen事件之前Laravel的生命周期已经完结，所以Laravel的Request是可读的，Session是可读写的
         // \Log::info('New WebSocket connection', [$request->fd, request()->all(), session()->getId(), session('xxx'), session(['yyy' => time()])]);
+        \Log::info('WebSocket 连接建立');
         $server->push($request->fd, 'Welcome to LaravelS');
         // throw new \Exception('an exception');// 此时抛出的异常上层会忽略，并记录到Swoole日志，需要开发者try/catch捕获处理
     }
+    //收到消息时触发
     public function onMessage(Server $server, Frame $frame)
     {
         // \Log::info('Received message', [$frame->fd, $frame->data, $frame->opcode, $frame->finish]);
         $server->push($frame->fd, date('Y-m-d H:i:s'));
         // throw new \Exception('an exception');// 此时抛出的异常上层会忽略，并记录到Swoole日志，需要开发者try/catch捕获处理
     }
+    //关闭消息时触发
     public function onClose(Server $server, $fd, $reactorId)
     {
+        \Log::info('WebSocket 连接关闭');
         // throw new \Exception('an exception');// 此时抛出的异常上层会忽略，并记录到Swoole日志，需要开发者try/catch捕获处理
     }
 }
