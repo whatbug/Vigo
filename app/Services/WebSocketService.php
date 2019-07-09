95eb956b6e7a0fe5bdb5e222d18b2dd51da1f50c
xthon <1873300303@qq.com>
2019-07-09 15:01:29 +0800

    socket

--- app/Services/WebSocketService.php
+++ app/Services/WebSocketService.php
@@ -1,39 +1,37 @@
 <?php namespace App\Services;
+
 use Hhxsv5\LaravelS\Swoole\WebSocketHandlerInterface;
+use Illuminate\Support\Facades\Log;
 use Swoole\Http\Request;
 use Swoole\WebSocket\Frame;
 use Swoole\WebSocket\Server;
-/**
- * @see https://wiki.swoole.com/wiki/page/400.html
- */
+
 class WebSocketService implements WebSocketHandlerInterface
 {
-    // 声明没有参数的构造函数
     public function __construct()
     {
 
     }
 
-    //建立时触发
+    // 连接建立时触发
     public function onOpen(Server $server, Request $request)
     {
-        // 在触发onOpen事件之前Laravel的生命周期已经完结，所以Laravel的Request是可读的，Session是可读写的
-        // \Log::info('New WebSocket connection', [$request->fd, request()->all(), session()->getId(), session('xxx'), session(['yyy' => time()])]);
-        \Log::info('WebSocket 连接建立');
-        $server->push($request->fd, 'Welcome to LaravelS');
-        // throw new \Exception('an exception');// 此时抛出的异常上层会忽略，并记录到Swoole日志，需要开发者try/catch捕获处理
+        // 在触发 WebSocket 连接建立事件之前，Laravel 应用初始化的生命周期已经结束，你可以在这里获取 Laravel 请求和会话数据
+        // 调用 push 方法向客户端推送数据，fd 是客户端连接标识字段
+        Log::info('WebSocket 连接建立');
+        $server->push($request->fd, 'Welcome to WebSocket Server built on LaravelS');
     }
-    //收到消息时触发
+
+    // 收到消息时触发
     public function onMessage(Server $server, Frame $frame)
     {
-        // \Log::info('Received message', [$frame->fd, $frame->data, $frame->opcode, $frame->finish]);
-        $server->push($frame->fd, date('Y-m-d H:i:s'));
-        // throw new \Exception('an exception');// 此时抛出的异常上层会忽略，并记录到Swoole日志，需要开发者try/catch捕获处理
+        // 调用 push 方法向客户端推送数据
+        $server->push($frame->fd, 'This is a message sent from WebSocket Server at ' . date('Y-m-d H:i:s'));
     }
-    //关闭消息时触发
+
+    // 关闭连接时触发
     public function onClose(Server $server, $fd, $reactorId)
     {
-        \Log::info('WebSocket 连接关闭');
-        // throw new \Exception('an exception');// 此时抛出的异常上层会忽略，并记录到Swoole日志，需要开发者try/catch捕获处理
+        Log::info('WebSocket 连接关闭');
     }
 }
