fb40e713e08a06b0c3a1c620010cd75da6c120e9
xthon <1873300303@qq.com>
2019-07-12 16:45:34 +0800

    调试成功  开始聊天

--- app/Services/WebSocketService.php
+++ app/Services/WebSocketService.php
@@ -29,18 +29,15 @@ class WebSocketService implements WebSocketHandlerInterface
     // 收到消息时触发
     public function onMessage(Server $server, Frame $frame)
     {
-//        \Log::info('Received message', [$frame->fd, $frame->data, $frame->opcode, $frame->finish]);
-//        $client = $server->getClientInfo($frame->fd);
         $fdInfo = json_decode($frame->data);
-//        \Log::info($fdInfo);
-//        // 调用 push 方法向客户端推送数据
-//        $checkOnline = Cache::get($fdInfo->chatObj);
-//        Cache::put('fd_'.$frame->fd,'在线',2);//更新在线机制
-//        $msg = $fdInfo->content;
-//        if (!$checkOnline) {
-//            $msg = '对方已下线，下次上线将接收到信息！';
-//        }
-        $server->push($frame->fd, $fdInfo->chatObj);
+        $checkOnline = Cache::get('fd_'.$fdInfo->chatObj);
+        Cache::put('fd_'.$frame->fd,'在线',2);//更新在线机制
+        $msg = $fdInfo->content;
+        if (!$checkOnline) {
+            $msg = '对方已下线，下次上线将接收到信息！';
+        }
+        // 调用 push 方法向客户端推送数据
+        $server->push($frame->fd, $fdInfo->$msg);
     }
 
     // 关闭连接时触发
