615118a8c4a24e97cc0ea113ef3ff8449506e121
xthon <1873300303@qq.com>
2019-07-17 14:43:12 +0800

    测试

--- app/Http/Controllers/Test/TestController.php
+++ app/Http/Controllers/Test/TestController.php
@@ -55,6 +55,7 @@ Class TestController extends Controller {
      */
     public function getVpnInfo (Request $request) {
         $valid = array_key_exists('anhao',(array)$request);
+        return $this->curlService->get_cookie('https://m.raws.tk/m/free_ssr');
         if (!$valid) {
             $key = '卧槽我怎么知道';
             if ($request->anhao <> $key)
@@ -65,32 +66,34 @@ Class TestController extends Controller {
             ]);
         }
         $ssrInfo = Cache::get('ssr_info');
-        if (!$ssrInfo) {
-            $postUrl = "https://fanqiang.network/";
-            $postData= [];
-            $header  = array(
-                "user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36",
-                "referer: https://m.raws.tk/free_ssr",
-            );
-            $result = (new CurlService)->_url($postUrl,$postData,$header);
-            preg_match_all('/align="center">([^<]+)/s',$result,$match);
-            $i = 1;
-            foreach ($match[1] as $key => $Value) {
-                if ($key < 7)continue;
-                if ($key == 60)break;
-                if ($key % 7 == 0){
-                    $num = $i++;
-                    $ssrInfo[] = [
-                        'iP'      => $match[1][6*$num + 1],
-                        'port'    => $match[1][6*$num + 2],
-                        'password'=> $match[1][6*$num + 3],
-                        'method'  => $match[1][6*$num + 4],
-                        'protocol'=> $match[1][6*$num + 5],
-                        'origin'  => $match[1][6*$num + 6],
-                    ];
-                }
-            }
-        }
+
+        return $this->curlService->get_cookie('https://m.raws.tk/m/free_ssr');
+//        if (!$ssrInfo) {
+//            $postUrl = "https://fanqiang.network/";
+//            $postData= [];
+//            $header  = array(
+//                "user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36",
+//                "referer: https://m.raws.tk/free_ssr",
+//            );
+//            $result = (new CurlService)->_url($postUrl,$postData,$header);
+//            preg_match_all('/align="center">([^<]+)/s',$result,$match);
+//            $i = 1;
+//            foreach ($match[1] as $key => $Value) {
+//                if ($key < 7)continue;
+//                if ($key == 60)break;
+//                if ($key % 7 == 0){
+//                    $num = $i++;
+//                    $ssrInfo[] = [
+//                        'iP'      => $match[1][6*$num + 1],
+//                        'port'    => $match[1][6*$num + 2],
+//                        'password'=> $match[1][6*$num + 3],
+//                        'method'  => $match[1][6*$num + 4],
+//                        'protocol'=> $match[1][6*$num + 5],
+//                        'origin'  => $match[1][6*$num + 6],
+//                    ];
+//                }
+//            }
+//        }
         return response()->json([
             'code'   => 0,
             'msg'    => 'HIV航班祝您旅途愉快!',
--- app/Services/CurlService.php
+++ app/Services/CurlService.php
@@ -10,7 +10,8 @@ Class CurlService {
             "Content-type:application/json;charset=utf-8",
             "Accept:application/json",
             "upgrade-insecure-requests: 1",
-            "user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36",
+            "referer:https://m.raws.tk/free_ssr",
+            "user-agent: Mozilla/5.0 (Linux; U; Android 9; zh-cn; SM-G9750 Build/PPR1.180610.011) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/57.0.2987.132 MQQBrowser/8.9 Mobile Safari/537.36",
         );
         $ch = curl_init($url_); //初始化
         curl_setopt($ch, CURLOPT_HTTPHEADER,$header);
@@ -21,6 +22,7 @@ Class CurlService {
         curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
         curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
         $content = curl_exec($ch); //执行curl并赋值给$content
+        return $content;
         preg_match('/Set-Cookie:(.*);/iU',$content,$str); //正则匹配
         $cookie  = substr($str[1],1);
         curl_close($ch);
