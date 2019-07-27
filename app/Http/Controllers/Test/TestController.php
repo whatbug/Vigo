83b19575bd97c7cc223f88c02060f4536f3215b9
xthon <1873300303@qq.com>
2019-07-27 16:41:38 +0800

    go

--- app/Http/Controllers/Test/TestController.php
+++ app/Http/Controllers/Test/TestController.php
@@ -101,21 +101,26 @@ Class TestController extends Controller {
 
 
     public function cll (){
-        $resource = file_get_contents(base_path()."/storage/ssr.txt");$i = 1;
+        $resource = file_get_contents(base_path()."/storage/ssr.txt");$i = 0;
         preg_match_all('/align="center">([^<]+)/s',$resource,$match);
+        $array = array_values(array_splice($match[1],5));
         if (sizeof($match[1])) {
-            foreach ($match[1] as $key => $Value) {
-                if ($key % 7 == 0) {
+            foreach ($array as $key => $Value) {
+                if (($key + 1) % 6 == 0) {
                     $num = $i++;
+                    $rematch = file_get_contents("http://freeapi.ipip.net/{$array[6 * $num + 0]}");
+                    preg_match_all("/([\x{4e00}-\x{9fa5}]+)/u",$rematch,$country);
                     $redData[] = [
-                        'iP'       => $match[1][6 * $num + 1],
-                        'port'     => $match[1][6 * $num + 2],
-                        'password' => $match[1][6 * $num + 3],
-                        'method'   => $match[1][6 * $num + 4],
-                        'protocol' => $match[1][6 * $num + 5],
-                        'origin'   => $match[1][6 * $num + 6],
+                        'service'  => $array[6 * $num + 0],
+                        'port'     => $array[6 * $num + 1],
+                        'password' => $array[6 * $num + 2],
+                        'method'   => $array[6 * $num + 3],
+                        'check_at' => $array[6 * $num + 4],
+                        'country'  => ($country[0][0] == '中国')?$country[0][1]:$country[0][0],
+                        'status'   => '有效',
                     ];
                 }
+
             }
         }
         return $redData;
