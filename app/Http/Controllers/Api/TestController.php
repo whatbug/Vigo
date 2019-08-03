86ce3d12285d23d0dfc24b58600425187ffca32a
xthon <1873300303@qq.com>
2019-08-03 17:39:10 +0800

    cookie

--- app/Http/Controllers/Api/TestController.php
+++ app/Http/Controllers/Api/TestController.php
@@ -4,13 +4,14 @@ namespace App\Http\Controllers\Api;
 
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Support\Facades\Cookie;
 
 class TestController extends BaseController
 {
     //验证密码正确性
     public function vpnValidate (Request $request) {
         if ($request->anhao == '卧槽我怎么知道'){
-            setcookie('anhao',md5('卧槽我怎么知道'),time()+86000,'/');
+            Cookie::queue('anhao',md5('卧槽我怎么知道'),24*3600);
             return 1;
         }
         return 0;
