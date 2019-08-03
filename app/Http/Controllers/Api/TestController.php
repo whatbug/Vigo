0c38292bc9916d7b0094012219311e7f6b2ec303
xthon <1873300303@qq.com>
2019-08-03 17:32:34 +0800

    expires

--- app/Http/Controllers/Api/TestController.php
+++ app/Http/Controllers/Api/TestController.php
@@ -10,7 +10,7 @@ class TestController extends BaseController
     //验证密码正确性
     public function vpnValidate (Request $request) {
         if ($request->anhao == '卧槽我怎么知道'){
-            setcookie('anhao',md5('卧槽我怎么知道'));
+            setcookie('anhao',md5('卧槽我怎么知道'),time()+86000,'/');
             return 1;
         }
         return 0;
