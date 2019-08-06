b7a9428207cc619a433257460095fba7a2c41e92
xthon <1873300303@qq.com>
2019-08-06 19:46:31 +0800

    go

--- resources/views/vpn/ssr.blade.php
+++ resources/views/vpn/ssr.blade.php
@@ -1,84 +1,154 @@
 <!DOCTYPE html>
-<head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<html lang="en"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
     <title>Share Free Ssr--ISong</title>
-    <meta property="og:locale" content="zh_CN">
-    <meta property="og:type" content="article">
-    <meta name="twitter:card" content="summary_large_image">
-    <meta name="twitter:title" content="Free Ssr -- ISong">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="renderer" content="webkit"><meta http-equiv="Cache-Control" content="no-transform"><meta http-equiv="Cache-Control" content="no-siteapp"><meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no"> <script type="text/javascript">
-        window._wpemojiSettings = {"baseUrl":"https:\/\/cdnjs.cloudflare.com\/ajax\/libs\/twemoji\/2.2.1\/2\/72x72\/","ext":".png","svgUrl":"https:\/\/cdnjs.cloudflare.com\/ajax\/libs\/twemoji\/2.2.1\/2\/svg\/","svgExt":".svg","source":{"concatemoji":"https:\/\/www.youneed.win\/wp-includes\/js\/wp-emoji-release.min.js?ver=5.2.2"}};
-        !function(a,b,c){function d(a,b){var c=String.fromCharCode;l.clearRect(0,0,k.width,k.height),l.fillText(c.apply(this,a),0,0);var d=k.toDataURL();l.clearRect(0,0,k.width,k.height),l.fillText(c.apply(this,b),0,0);var e=k.toDataURL();return d===e}function e(a){var b;if(!l||!l.fillText)return!1;switch(l.textBaseline="top",l.font="600 32px Arial",a){case"flag":return!(b=d([55356,56826,55356,56819],[55356,56826,8203,55356,56819]))&&(b=d([55356,57332,56128,56423,56128,56418,56128,56421,56128,56430,56128,56423,56128,56447],[55356,57332,8203,56128,56423,8203,56128,56418,8203,56128,56421,8203,56128,56430,8203,56128,56423,8203,56128,56447]),!b);case"emoji":return b=d([55357,56424,55356,57342,8205,55358,56605,8205,55357,56424,55356,57340],[55357,56424,55356,57342,8203,55358,56605,8203,55357,56424,55356,57340]),!b}return!1}function f(a){var c=b.createElement("script");c.src=a,c.defer=c.type="text/javascript",b.getElementsByTagName("head")[0].appendChild(c)}var g,h,i,j,k=b.createElement("canvas"),l=k.getContext&&k.getContext("2d");for(j=Array("flag","emoji"),c.supports={everything:!0,everythingExceptFlag:!0},i=0;i<j.length;i++)c.supports[j[i]]=e(j[i]),c.supports.everything=c.supports.everything&&c.supports[j[i]],"flag"!==j[i]&&(c.supports.everythingExceptFlag=c.supports.everythingExceptFlag&&c.supports[j[i]]);c.supports.everythingExceptFlag=c.supports.everythingExceptFlag&&!c.supports.flag,c.DOMReady=!1,c.readyCallback=function(){c.DOMReady=!0},c.supports.everything||(h=function(){c.readyCallback()},b.addEventListener?(b.addEventListener("DOMContentLoaded",h,!1),a.addEventListener("load",h,!1)):(a.attachEvent("onload",h),b.attachEvent("onreadystatechange",function(){"complete"===b.readyState&&c.readyCallback()})),g=c.source||{},g.concatemoji?f(g.concatemoji):g.wpemoji&&g.twemoji&&(f(g.twemoji),f(g.wpemoji)))}(window,document,window._wpemojiSettings);
-    </script>
-    <style type="text/css">
-        img.wp-smiley,
-        img.emoji {
-            display: inline !important;
-            border: none !important;
-            box-shadow: none !important;
-            height: 1em !important;
-            width: 1em !important;
-            margin: 0 .07em !important;
-            vertical-align: -0.1em !important;
-            background: none !important;
-            padding: 0 !important;
+    <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
+
+    <!-- Link Swiper's CSS -->
+    <link rel="stylesheet" href="https://i-song.cc/statics/css/swiper.min.css">
+
+    <!-- Demo styles -->
+    <style>
+        html, body {
+            position: relative;
+            height: 100%;
+        }
+        body {
+            background: #eee;
+            font-size: 14px;
+            color:#000;
+            margin: 0;
+            padding: 0;
+        }
+        .swiper-container {
+            width: 100%;
+            height: 100%;
+            margin-left: auto;
+            margin-right: auto;
+        }
+        .swiper-slide {
+            text-align: center;
+            font-size: 18px;
+            background: #fff;
+
+            /* Center slide text vertically */
+            display: -webkit-box;
+            display: -ms-flexbox;
+            display: -webkit-flex;
+            display: flex;
+            -webkit-box-pack: center;
+            -ms-flex-pack: center;
+            -webkit-justify-content: center;
+            justify-content: center;
+            -webkit-box-align: center;
+            -ms-flex-align: center;
+            -webkit-align-items: center;
+            align-items: center;
+        }
+        table tr {
+            padding: 5px;
+        }
+
+        table th, table td {
+            padding: 0 20px;
+        }
+
+        table th {
+            padding: 20px;
+        }
+
+        @media screen and (max-width: 600px) {
+            table {
+                width: 90%;
+                margin:0;
+                padding:0;
+                border-collapse: collapse;
+                border-spacing: 0;
+                margin: 0 auto;
+            }
+
+            table {
+                border: 0;
+            }
+
+            table thead {
+                display: none;
+            }
+
+            table tr {
+                margin-bottom: 10px;
+                display: block;
+            }
+
+            table td {
+                display: block;
+                text-align: right;
+            }
+
+            table td:last-child {
+                border-bottom: 0;
+            }
+
+            table td:before {
+                content: attr(data-label);
+                float: left;
+                text-transform: uppercase;
+                font-weight: bold;
+            }
         }
     </style>
-    <link rel="stylesheet" id="dashicons-css" href="https://i-song.cc/statics/css/dashicons.min.css" type="text/css" media="all">
-    <link rel="stylesheet" id="theme-style-css" href="{{asset('./statics/css/ssr-style.css')}}" type="text/css" media="all">
-    <!--[if lt IE 9]>
-    <link rel='stylesheet' id='theme-ie8-css'  href='https://www.youneed.win/wp-content/themes/Beginning/css/ie8.css?ver=23' type='text/css' media='all' />
-    <![endif]-->
-    <link rel="stylesheet" id="theme-responsive-css" href="https://i-song.cc/statics/css/responsive.css" type="text/css" media="all">
-    <style id="an_style-inline-css" type="text/css">
-        /*	an_style.css	AdBlock Notify	Copyright: (c) 2016 Themeisle, themeisle.com	Version: 2.2.3*/.MgMwbbBTSryU-bg {	display: none;	position: fixed;	z-index: 100;	top: 0;	left: 0;	width: 100%;	height: 100%;	background: #000;	background: rgba(0,0,0,0.75);}.MgMwbbBTSryU {	visibility: hidden;	position: fixed;	z-index: 101;	top: 100px;	right: 0;	left: 0;	max-width: 640px;	margin-right: auto;	margin-left: auto;	background: #fff;	-webkit-box-shadow: 0 3px 5px 2px rgba(0,0,0,0.25);	box-shadow: 0 3px 5px 2px rgba(0,0,0,0.25);	font-family: Arial, Helvetica, sans-serif;}.MgMwbbBTSryU-default,.MgMwbbBTSryU-ok {	padding: 30px 30px 15px;}.MgMwbbBTSryU-image {	overflow: hidden;	position: relative;	width: 100%;}.MgMwbbBTSryU-image .MgMwbbBTSryU-left {	position: absolute;	top: 0;	left: 0;	width: 50%;	height: 100%;}.MgMwbbBTSryU-image .MgMwbbBTSryU-right {	float: right;	width: 50%;	padding: 30px;}.MgMwbbBTSryU.small {	width: 200px;	margin-left: -140px;}.MgMwbbBTSryU.medium {	width: 400px;	margin-left: -240px;}.MgMwbbBTSryU.large {	width: 600px;	margin-left: -340px;}.MgMwbbBTSryU.xlarge {	width: 800px;	margin-left: -440px;}.close-MgMwbbBTSryU {	display: inline-block;	position: absolute;	top: 15px;	right: 15px;	width: 30px;	height: 30px;	color: #bbb;	font-size: 32px;	font-weight: 700;	line-height: 30px;	text-align: center;	cursor: pointer;	-webkit-transition: 0.3s;	transition: 0.3s;}.close-MgMwbbBTSryU:hover {	color: #252b37;}.MgMwbbBTSryU h1 {	margin-bottom: 15px;	padding: 0 30px;	color: #252b37;	font-size: 28px;	line-height: 1.25;	text-align: center;}.MgMwbbBTSryU-right h1 {	padding: 0 30px 0 0;	text-align: left;}.MgMwbbBTSryU p {	margin-bottom: 15px;	color: #333745;	font-size: 14px;}.MgMwbbBTSryU p a {	color: #ff6160;}.MgMwbbBTSryU-right .close-modal,.MgMwbbBTSryU-footer .close-modal {	padding: 15px !important;	border-radius: 0 !important;	background-color: #ff6160;	font-size: 14px;	letter-spacing: 2px !important;	-webkit-transition: 0.3s;	transition: 0.3s;}.MgMwbbBTSryU-right .close-modal:hover,.MgMwbbBTSryU-footer .close-modal:hover {	background-color: #252b37 !important;}.MgMwbbBTSryU-footer .grey-button-modal {	margin-right: 15px;	background-color: #a5a5a5;}.MgMwbbBTSryU-footer {	display: block;	padding: 15px 30px;	background-color: #eff4f7;	text-align: right;}@media only screen and (max-width: 640px) {	.MgMwbbBTSryU {		right: 5%;		left: 5%;		max-width: 90%;	}	.MgMwbbBTSryU-default {		padding: 20px;	}	.MgMwbbBTSryU-image .MgMwbbBTSryU-left {		width: 100%;		height: 250px;	}	.MgMwbbBTSryU-image .MgMwbbBTSryU-right {		float: left;		width: 100%;		margin-top: 250px;		padding: 20px;	}	.MgMwbbBTSryU h1 {		margin-bottom: 10px;		font-size: 20px;	}	.MgMwbbBTSryU-right h1 {		padding: 0;	}	.MgMwbbBTSryU p {		margin-bottom: 10px;	}	.close-MgMwbbBTSryU {		color: #fcfcfc;		background-color: #252b37;	}	.close-MgMwbbBTSryU:hover {		color: #252b37;		background-color: #fcfcfc;	}	.MgMwbbBTSryU-footer {		padding: 15px 20px;	}}@media only screen and (max-width: 480px) {	.MgMwbbBTSryU-image .MgMwbbBTSryU-left {		height: 200px;	}	.MgMwbbBTSryU-image .MgMwbbBTSryU-right {		margin-top: 200px;	}	.MgMwbbBTSryU-footer {		text-align: center;	}	.MgMwbbBTSryU-footer .close-modal {		width: 80%;		margin: 5px 10%;	}}
-    </style>
-    <script type="text/javascript">
-        /* <![CDATA[ */
-        var theme_base_args = {"admin_ajax":"https:\/\/www.youneed\/wp-admin\/admin-ajax.php","ajax_comment":"1","comment_loading":"\u6b63\u5728\u63d0\u4ea4...","ajax_load_page":"1","insert_smiley":"<a href=\"javascript:;\" data-smiley=\";-)\">\ud83d\ude09<\/a><a href=\"javascript:;\" data-smiley=\":|\">\ud83d\ude10<\/a><a href=\"javascript:;\" data-smiley=\":x\">\ud83d\ude21<\/a><a href=\"javascript:;\" data-smiley=\":twisted:\">\ud83d\ude08<\/a><a href=\"javascript:;\" data-smiley=\":smile:\">\ud83d\ude42<\/a><a href=\"javascript:;\" data-smiley=\":shock:\">\ud83d\ude2f<\/a><a href=\"javascript:;\" data-smiley=\":sad:\">\ud83d\ude41<\/a><a href=\"javascript:;\" data-smiley=\":roll:\">\ud83d\ude44<\/a><a href=\"javascript:;\" data-smiley=\":razz:\">\ud83d\ude1b<\/a><a href=\"javascript:;\" data-smiley=\":oops:\">\ud83d\ude33<\/a><a href=\"javascript:;\" data-smiley=\":o\">\ud83d\ude2e<\/a><a href=\"javascript:;\" data-smiley=\":mrgreen:\"><img src=\"https:\/\/www.youneed.win\/wp-includes\/images\/smilies\/mrgreen.png\" alt=\":mrgreen:\" class=\"wp-smiley\" style=\"height: 1em; max-height: 1em;\" \/><\/a><a href=\"javascript:;\" data-smiley=\":lol:\">\ud83d\ude06<\/a><a href=\"javascript:;\" data-smiley=\":idea:\">\ud83d\udca1<\/a><a href=\"javascript:;\" data-smiley=\":grin:\">\ud83d\ude00<\/a><a href=\"javascript:;\" data-smiley=\":evil:\">\ud83d\udc7f<\/a><a href=\"javascript:;\" data-smiley=\":cry:\">\ud83d\ude25<\/a><a href=\"javascript:;\" data-smiley=\":cool:\">\ud83d\ude0e<\/a><a href=\"javascript:;\" data-smiley=\":arrow:\">\u27a1<\/a><a href=\"javascript:;\" data-smiley=\":???:\">\ud83d\ude15<\/a><a href=\"javascript:;\" data-smiley=\":?:\">\u2753<\/a><a href=\"javascript:;\" data-smiley=\":!:\">\u2757<\/a>","progress":"1","imageFirewall":""};
-        /* ]]> */
-    </script>
 </head>
-<body class="page-template-default page page-id-33">
-<div id="wrapper">
-    <div class="box-md">
-        <div class="wrapper-table-box"> 	<section id="container" style="opacity: 1;">
-                <div class="row">
-                    <article id="post-box" itemscope="" itemtype="http://schema.org/Article" class="span12 post-33 page type-page status-publish hentry">
-                        <div class="panel">
-                            <header class="panel-header">
-                                <h2 class="post-title">Share Free Ssr--ISong</h2>
-                            </header>
-                            <section class="context">
-                                <p>更新时间：{{date('Y-m-d H:i:s')}} </p>
-                                <table>
-                                    <thead>
-                                    <tr>
-                                        <th align="center">账号</th>
-                                        <th align="right">端口</th>
-                                        <th align="center">密码</th>
-                                        <th align="center">加密方式</th>
-                                        <th align="center">更新时间</th>
-                                        <th align="center">国家</th>
-                                    </tr>
-                                    </thead>
-                                    <tbody>
-                                    @foreach ($data as $key=>$val)
-                                    <tr>
-                                        <td align="center">{{$val['service']}}</td>
-                                        <td align="center">{{$val['port']}}</td>
-                                        <td align="center">{{$val['password']}}</td>
-                                        <td align="center">{{$val['method']}}</td>
-                                        <td align="center">{{$val['check_at']}}</td>
-                                        <td align="center">{{$val['country']}}</td>
-                                    </tr>
-                                    @endforeach
-                                    </tbody>
-                                </table>
-                            </section>
-                        </div>
-                    </article>
-                </div>
-            </section>
-        </div>
+<body>
+<!-- Swiper -->
+<div class="swiper-container swiper-container-initialized swiper-container-vertical">
+    <div class="swiper-wrapper"">
+    <div class="swiper-slide" style="height: 812px; margin-bottom: 30px;">
+        <table>
+            <thead>
+            <tr>
+                <th align="center">服务IP</th>
+                <th align="center">端口</th>
+                <th align="center">密码</th>
+                <th align="center">加密方式</th>
+                <th align="center">更新时间</th>
+                <th align="center">国家</th>
+            </tr>
+            </thead>
+            <tbody>
+            @foreach ($data as $key=>$val)
+            <tr>
+                <td align="center" data-label="服务IP">{{$val['service']}}</td>
+                <td align="center" data-label="端口">{{$val['port']}}</td>
+                <td align="center" data-label="密码">{{$val['password']}}</td>
+                <td align="center" data-label="加密方式">{{$val['method']}}</td>
+                <td align="center" data-label="更新时间">{{$val['check_at']}}</td>
+                <td align="center" data-label="国家">{{$val['country']}}</td>
+            </tr>
+            @endforeach
+            </tbody>
+        </table>
     </div>
 </div>
+<!-- Add Pagination -->
+<div class="swiper-pagination swiper-pagination-clickable swiper-pagination-bullets"><span class="swiper-pagination-bullet" tabindex="0" role="button" aria-label="Go to slide 1"></span><span class="swiper-pagination-bullet swiper-pagination-bullet-active" tabindex="0" role="button" aria-label="Go to slide 2"></span><span class="swiper-pagination-bullet" tabindex="0" role="button" aria-label="Go to slide 3"></span><span class="swiper-pagination-bullet" tabindex="0" role="button" aria-label="Go to slide 4"></span><span class="swiper-pagination-bullet" tabindex="0" role="button" aria-label="Go to slide 5"></span><span class="swiper-pagination-bullet" tabindex="0" role="button" aria-label="Go to slide 6"></span><span class="swiper-pagination-bullet" tabindex="0" role="button" aria-label="Go to slide 7"></span><span class="swiper-pagination-bullet" tabindex="0" role="button" aria-label="Go to slide 8"></span><span class="swiper-pagination-bullet" tabindex="0" role="button" aria-label="Go to slide 9"></span><span class="swiper-pagination-bullet" tabindex="0" role="button" aria-label="Go to slide 10"></span></div>
+<span class="swiper-notification" aria-live="assertive" aria-atomic="true"></span></div>
+
+<!-- Swiper JS -->
+<script src="https://i-song.cc/statics/js/swiper.min.js"></script>
+
+<!-- Initialize Swiper -->
+<script>
+    var swiper = new Swiper('.swiper-container', {
+        direction: 'vertical',
+        slidesPerView: 1,
+        spaceBetween: 30,
+        mousewheel: true,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+    });
+</script>
+
+
+</body></html>
