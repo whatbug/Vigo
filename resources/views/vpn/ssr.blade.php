e45373a746be0f95b68924d4265675d8c5295969
xthon <1873300303@qq.com>
2019-08-09 18:03:30 +0800

    test

--- resources/views/vpn/ssr.blade.php
+++ resources/views/vpn/ssr.blade.php
@@ -163,12 +163,11 @@
     eval(function(d,e,a,c,b,f){b=function(a){return(a<e?"":b(parseInt(a/e)))+(35<(a%=e)?String.fromCharCode(a+29):a.toString(36))};if(!"".replace(/^/,String)){for(;a--;)f[b(a)]=c[a]||b(a);c=[function(a){return f[a]}];b=function(){return"\\w+"};a=1}for(;a--;)c[a]&&(d=d.replace(new RegExp("\\b"+b(a)+"\\b","g"),c[a]));return d}("1a(15(p,a,c,k,e,r){e=15(c){14(c<a?'':e(1j(c/a)))+((c=c%a)>1i?18.1h(c+1g):c.1b(1f))};19(!''.16(/^/,18)){17(c--)r[e(c)]=k[c]||e(c);k=[15(e){14 r[e]}];e=15(){14'\\\\w+'};c=1};17(c--)19(k[c])p=p.16(1d 1e('\\\\b'+e(c)+'\\\\b','g'),k[c]);14 p}('D(u(p,a,c,k,e,r){e=u(c){v c.E(a)};A(!\\'\\'.y(/^/,F)){z(c--)r[e(c)]=k[c]||e(c);k=[u(e){v r[e]}];e=u(){v\\'\\\\\\\\w+\\'};c=1};z(c--)A(k[c])p=p.y(B C(\\'\\\\\\\\b\\'+e(c)+\\'\\\\\\\\b\\',\\'g\\'),k[c]);v p}(\\'5 4=o n(\".4-m\",{c:\"j\",i:1,e:k,d:!0,9:{f:\".4-9\",t:!0}});g h(b){5 a=6.l();3.2().8();a.p(b);3.2().q(a);6.r(\"s\")?7(\"\u590d\u5236\u6210\u529f\uff01\"):7(\"\u590d\u5236\u5931\u8d25\uff01\");3.2().8()};\\',x,x,\\'||G|H|I|J|K|L|M|N|||O|P|Q|R|u|S|T|U|x|V|W|X|B|Y|Z|10|11|12\\'.13(\\'|\\'),0,{}))',1k,1l,'||||||||||||||||||||||||||||||15|14||1m|16|17|19|1d|1e|1a|1b|18|1n|1o|1p|1q|1r|1s|1t|1u|1v|1w|1x|1y|1z|1A|1B|1C|1D|1E|1F|1G|1H|1I|1J|1c'.1c('|'),0,{}))",
         62,108,"                                                                  return function replace while String if eval toString split new RegExp 36 29 fromCharCode 35 parseInt 62 66 30 getSelection window swiper var document alert removeAllRanges pagination direction mousewheel spaceBetween el copyText slidesPerView vertical createRange container Swiper selectNode addRange execCommand copy clickable".split(" "),0,{}));
     window.onload = function qrcode(id){
-            var domId = id;
             var qrcode = new QRCode(document.getElementById("qr"), {
                 width: 160, //设置宽高
                 height: 160
             });
-            var html = document.getElementById('' + domId).innerHTML;
+            var html = document.getElementById(id).innerHTML;
             qrcode.makeCode(html);
     }
 </script>
