7a97f79a55baf37ed1c79aea795ca1235abca463
xthon <1873300303@qq.com>
2019-09-22 00:41:29 +0800

    go

--- app/Console/Commands/SpyDataNotify.php
+++ app/Console/Commands/SpyDataNotify.php
@@ -24,7 +24,7 @@ Class SpyDataNotify extends Command {
      * Execute the console command.
      * @return mixed
      */
-    public function handle(RecordData $data) {
+    public function handle(RunMethod $data) {
         set_time_limit(0);
         $content = shell_exec('python3 /spy.py');
         if (!is_null($content)) {
