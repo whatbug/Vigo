b21d87b8386171c02a8d583d87102331a4bc91ba
xthon <1873300303@qq.com>
2019-10-10 18:03:33 +0800

    go

--- app/Console/CronTab/SpyNotify.php
+++ app/Console/CronTab/SpyNotify.php
@@ -21,7 +21,7 @@ Class SpyNotify extends CronJob
 
    public function run()
    {
-       $content = shell_exec('python3 /spy.py >/dev/null 2>&1 &');
+       $content = shell_exec('python3 /spy.py >/dev/null &');
        if (!is_null($content)) {
            $dataNum = explode(',',$content);
            if (sizeof($dataNum)) {
