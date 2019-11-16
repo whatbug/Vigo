5bb63dfbf7b07329f216629744dc18a2028a8c5a
向某 <1873300303@qq.com>
2019-11-16 18:11:59 +0800

    go

--- app/Console/Commands/NewSSProcess.php
+++ app/Console/Commands/NewSSProcess.php
@@ -23,38 +23,76 @@ Class NewSSProcess extends Command {
      * @return mixed
      */
     public function handle() {
-        set_time_limit(0);$arr = array();
-        shell_exec("python3 /new.py");
-        $imgUrl = file_get_contents(base_path()."/storage/ss.txt");
-        $content = (new BaiOrcService())->resRecognize($imgUrl);
-        foreach ($content->words_result as $key => $val) {
-            if (preg_match('/:[\S]+/',str_replace(' ','',$val->words),$resMatch)){
-                $arr[] = substr($resMatch[0],1);
-            }else {
-                continue;
+        set_time_limit(0);$arr = array();$insertData = array();
+        if (date('H') == 2) {
+            shell_exec("python3 /new.py");
+            $imgUrl = file_get_contents(base_path() . "/storage/ss.txt");
+            $content = (new BaiOrcService())->resRecognize($imgUrl);
+            foreach ($content->words_result as $key => $val) {
+                if (preg_match('/:[\S]+/', str_replace(' ', '', $val->words), $resMatch)) {
+                    $arr[] = substr($resMatch[0], 1);
+                } else {
+                    continue;
+                }
             }
-        }
-        $i = 0;
-        foreach ($arr as $key => $value) {
-            if ($key < 6 * ($i+1)) {
-                $country = json_decode(file_get_contents("http://freeapi.ipip.net/{$arr[6 * $i]}"));
+            $i = 0;
+            foreach ($arr as $key => $value) {
+                if ($key < 6 * ($i + 1)) {
+                    $country = json_decode(file_get_contents("http://freeapi.ipip.net/{$arr[6 * $i]}"));
+                    $insertData[] = [
+                        'service' => $arr[6 * $i],
+                        'port' => $arr[6 * $i + 1],
+                        'protocol' => $arr[6 * $i + 4],
+                        'method' => $arr[6 * $i + 3] == 'origin' ? "RC4" : $arr[6 * $i + 3],
+                        'password' => $arr[6 * $i + 2],
+                        'ssLink' => 'ss://' . base64_encode((($arr[6 * $i + 3] == 'origin') ? "RC4" : $arr[6 * $i + 3]) . ':' . base64_encode($arr[6 * $i + 2]) . '@' . $arr[6 * $i] . ':' . $arr[6 * $i + 1]),
+                        'ssrLink' => 'ssr://' . base64_encode("{$arr[6 * $i]}:{$arr[6 * $i +1]}:{$arr[6 * $i +4]}:" . (($arr[6 * $i + 3] == "origin") ? "RC4" : $arr[6 * $i + 3]) . ":plain:" . base64_encode($arr[6 * $i + 2]) . "/?obfsparam=&protoparam=&remarks=" . base64_encode("失效坐等" . $i) . "&group=" . base64_encode('free share for I-Song')),
+                        'country' => ($country[0] != '中国') ? $country[0] : $country[0] . "({$country[1]})",
+                        'check_at' => date('H:i:s'),
+                    ];
+                    sleep(3);
+                    $i++;
+                }
+                if ($i == 2) break;
+            }
+        } else {
+            $postUrl = "https://lncn.org/api/lncn";
+            $time = time();$redData = [];
+            $postData= ['origin'=>'https://lncn.org'];
+            $header  = array(
+                "user-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.97 Safari/537.36",
+                "referer: https://lncn.org",
+                "accept: application/json, text/plain, */*",
+                "origin: https://lncn.org",
+                "referer: https://lncn.org",
+                "cookie: _ga=GA1.2.1987760110.{$time}; _gid=GA1.2.1058273580.{$time}; _gat_gtag_UA_132719745_1=1"
+            );
+            $resContent = json_decode((new CurlService)->_url($postUrl,$postData,$header));
+            if (!is_object($resContent)) {
+                return false;
+            }
+            $secretKey = '6512654323241236';
+            $ssrData   = openssl_decrypt($resContent->ssrs, 'aes-128-ecb', $secretKey, 2 );
+            preg_match_all("/(?:\[)(.*)(?:\])/i",$ssrData,$res);
+            foreach (json_decode($res[0][0]) as $val) {
+                $country = json_decode(file_get_contents("http://freeapi.ipip.net/{$val->ssr->ip}"));
                 $redData[] = [
-                'service' => $arr[6 * $i],
-                'port'    => $arr[6 * $i +1],
-                'protocol'=> $arr[6 * $i +4],
-                'method'  => $arr[6 * $i +3] == 'origin' ? "RC4" : $arr[6 * $i +3],
-                'password'=> $arr[6 * $i +2],
-                'ssLink'  => 'ss://' . base64_encode((($arr[6 * $i +3] == 'origin') ? "RC4" : $arr[6 * $i +3]) . ':' . base64_encode($arr[6 * $i +2]) . '@' . $arr[6 * $i] . ':' . $arr[6 * $i +1]),
-                'ssrLink' => 'ssr://' . base64_encode("{$arr[6 * $i]}:{$arr[6 * $i +1]}:{$arr[6 * $i +4]}:".(($arr[6 * $i +3] == "origin") ? "RC4" : $arr[6 * $i +3]).":plain:".base64_encode($arr[6 * $i +2])."/?obfsparam=&protoparam=&remarks=".base64_encode("失效坐等".$i)."&group=".base64_encode('free share for I-Song')),
-                'country' => ($country[0] != '中国') ? $country[0] : $country[0] . "({$country[1]})",
-                'check_at'=> date('H:i:s'),
-            ];
-                sleep(3);
-                $i++;
+                    'service'  => $val->ssr->ip,
+                    'port'     => $val->ssr->port,
+                    'protocol' => $val->ssr->protocol,
+                    'method'   => $val->ssr->method,
+                    'obfs'     => $val->ssr->obfs,
+                    'password' => $val->ssr->password,
+                    'ssLink'   => 'ss://' . base64_encode($val->ssr->method . ':' . $val->ssr->password . '@' . $val->ssr->ip . ':' . $val->ssr->port),
+                    'ssrLink'  => $val->ssrUrl,
+                    'country'  => ($country[0]!='中国')?$country[0]:$country[0]."({$country[1]})",
+                    'check_at' => date('H:i:s'),
+                ];
             }
-            if($i == 2) break;
+            $originSsr = Cache::get('ssr_info');
+            $insertData  = array_values(array_merge($originSsr,$redData));
         }
-        return Cache::put('ssr_info',$redData,now()->addMinutes(60*24));
+        return Cache::put('ssr_info',$insertData,now()->addMinutes(60*24));
     }
 
 }
--- app/Console/Commands/SsrService.php
+++ app/Console/Commands/SsrService.php
@@ -24,72 +24,32 @@ Class SsrService extends Command {
      */
     public function handle() {
         set_time_limit(0);$insertData = [];
-        if (date('H') == 2) {
-            $content = file_get_contents(base_path() . "/storage/ss.txt");
-            $ssrs = base64_decode($content);
-            $stBase = explode('ssr://', str_replace("\n", "", $ssrs));
-            $normol = array_splice($stBase, 1);
-            $dataList = array_values(array_unique($normol));
-            foreach ($dataList as $key => $value) {
-                if ($key == 0) continue;
-                if ($key == 16) break;
-                $real_rs = base64_decode($value);
-                $last_arr = explode(':', explode('/', mb_convert_encoding($real_rs, 'UTF-8', 'UTF-8'))[0]);
-                $country = json_decode(file_get_contents("http://freeapi.ipip.net/{$last_arr[0]}"));
-                $insertData[] = [
-                    'service' => $last_arr[0],
-                    'port' => $last_arr[1],
-                    'protocol' => $last_arr[2],
-                    'method' => $last_arr[3],
-                    'obfs' => $last_arr[4],
-                    'password' => base64_decode($last_arr[5]),
-                    'ssLink' => 'ss://' . base64_encode($last_arr[3] . ':' . base64_decode($last_arr[5]) . '@' . $last_arr[0] . ':' . $last_arr[1]),
-                    'ssrLink' => 'ssr://' . $value,
-                    'country' => ($country[0] != '中国') ? $country[0] : $country[0] . "({$country[1]})",
-                    'check_at' => date('H:i:s'),
-                ];
-                sleep(3);
-            }
-            Cache::put('ssr_info',$insertData,60*60*6);
-        }  else  {
-            $postUrl = "https://lncn.org/api/lncn";
-            $time = time();$redData = [];
-            $postData= ['origin'=>'https://lncn.org'];
-            $header  = array(
-                "user-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.97 Safari/537.36",
-                "referer: https://lncn.org",
-                "accept: application/json, text/plain, */*",
-                "origin: https://lncn.org",
-                "referer: https://lncn.org",
-                "cookie: _ga=GA1.2.1987760110.{$time}; _gid=GA1.2.1058273580.{$time}; _gat_gtag_UA_132719745_1=1"
-            );
-            $resContent = json_decode((new CurlService)->_url($postUrl,$postData,$header));
-            if (!is_object($resContent)) {
-                return false;
-            }
-            $secretKey = '6512654323241236';
-            $ssrData   = openssl_decrypt($resContent->ssrs, 'aes-128-ecb', $secretKey, 2 );
-            preg_match_all("/(?:\[)(.*)(?:\])/i",$ssrData,$res);
-            foreach (json_decode($res[0][0]) as $val) {
-                $country = json_decode(file_get_contents("http://freeapi.ipip.net/{$val->ssr->ip}"));
-                $redData[] = [
-                    'service'  => $val->ssr->ip,
-                    'port'     => $val->ssr->port,
-                    'protocol' => $val->ssr->protocol,
-                    'method'   => $val->ssr->method,
-                    'obfs'     => $val->ssr->obfs,
-                    'password' => $val->ssr->password,
-                    'ssLink'   => 'ss://' . base64_encode($val->ssr->method . ':' . $val->ssr->password . '@' . $val->ssr->ip . ':' . $val->ssr->port),
-                    'ssrLink'  => $val->ssrUrl,
-                    'country'  => ($country[0]!='中国')?$country[0]:$country[0]."({$country[1]})",
-                    'check_at' => date('H:i:s'),
-                ];
-            }
-            $originSsr = Cache::get('ssr_info');
-            $insertData  = array_values(array_merge($originSsr,$redData));
-            Cache::put('ssr_info',$insertData,60*60*20);
+        $content = file_get_contents(base_path() . "/storage/ss.txt");
+        $ssrs = base64_decode($content);
+        $stBase = explode('ssr://', str_replace("\n", "", $ssrs));
+        $normol = array_splice($stBase, 1);
+        $dataList = array_values(array_unique($normol));
+        foreach ($dataList as $key => $value) {
+            if ($key == 0) continue;
+            if ($key == 16) break;
+            $real_rs = base64_decode($value);
+            $last_arr = explode(':', explode('/', mb_convert_encoding($real_rs, 'UTF-8', 'UTF-8'))[0]);
+            $country = json_decode(file_get_contents("http://freeapi.ipip.net/{$last_arr[0]}"));
+            $insertData[] = [
+                'service' => $last_arr[0],
+                'port' => $last_arr[1],
+                'protocol' => $last_arr[2],
+                'method' => $last_arr[3],
+                'obfs' => $last_arr[4],
+                'password' => base64_decode($last_arr[5]),
+                'ssLink' => 'ss://' . base64_encode($last_arr[3] . ':' . base64_decode($last_arr[5]) . '@' . $last_arr[0] . ':' . $last_arr[1]),
+                'ssrLink' => 'ssr://' . $value,
+                'country' => ($country[0] != '中国') ? $country[0] : $country[0] . "({$country[1]})",
+                'check_at' => date('H:i:s'),
+            ];
+            sleep(3);
         }
-        return true;
+        Cache::put('ssr_info',$insertData,60*60*6);
     }
 
 }
